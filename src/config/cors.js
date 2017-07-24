f369a5ceec0670d0b90bc600a0f9df5aca963f69
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2017-07-24 16:43:30 -0300

    Correção
    Acces -> Access

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,8 +1,8 @@
 /*isso é um middleware, o res é de 'response' */
 module.exports=function(req,res,next){
-    res.header('Acces-Control-Allow-Origin','*')
-    res.header('Acces-Control-Allow-Methods','GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    res.header('Acces-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept')
+    res.header('Access-Control-Allow-Origin','*')
+    res.header('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept')
 
     /*depois de inserir os cabeçalhos, se chama o next() */
     next()
